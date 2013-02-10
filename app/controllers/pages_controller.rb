06895958e347c9401a961675bcc97d420e84688c
Misha <misha.skubenichc@gmail.com>
2013-02-10 12:08:52 +0200

    add google instruments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,9 @@ class PagesController < ApplicationController
     @galleria_news = News.all(:conditions => ["DATE(created_at) > DATE(?)", Time.now - 1.month], :order => 'view_count DESC', :limit => 5)
     @last_news = News.all(:limit => 7, :order => 'created_at DESC', :select => "title, created_at, view_count, id")
     @last_articles = Article.all(:limit => 4, :order => 'created_at DESC', :select => "title, created_at, view_count, id")
-    @tags = ActiveRecord::Base.connection.select_all("SELECT tags.id, tags.title, (SELECT count(id) FROM news_tags WHERE tag_id = tags.id)+(SELECT count(id) FROM articles_tags WHERE tag_id = tags.id) AS totalcount
+    @tags = ActiveRecord::Base.connection.select_all("SELECT tags.id, tags.title,
+                                                             (SELECT count(id) FROM news_tags WHERE tag_id = tags.id)+
+                                                             (SELECT count(id) FROM articles_tags WHERE tag_id = tags.id) AS totalcount
                             FROM tags
                             ORDER BY totalcount DESC LIMIT 20;")
     max_size = 30

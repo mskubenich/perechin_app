a1fa885784901293d4d002956e36edcf86587412
Misha <misha.skubenichc@gmail.com>
2013-03-09 13:01:35 +0200

    authors list in art

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -4,5 +4,7 @@ class ArtsController < ApplicationController
     @art_categories = ArtCategory.all
     @category = ArtCategory.find(params[:art_category]) if params[:art_category]
     @art_subcategory = ArtSubcategory.find(params[:art_subcategory]) if params[:art_subcategory]
+    role_author = Role.find_by_name("author")
+    @authors = User.where(:role_id => role_author.id)
   end
 end

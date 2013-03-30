3e81a5d6b9faae21cf6a0b5a30817e2e963b41dd
mskubenich <misha.skubenich@gmail.com>
2013-03-30 17:46:04 +0200

    anecdotes part2

--- app/controllers/anecdotes_controller.rb
+++ app/controllers/anecdotes_controller.rb
@@ -17,7 +17,7 @@ class AnecdotesController < ApplicationController
       page =  Nokogiri::HTML(params[:anecdote][:body])
       page.xpath("//img[@asset]").each do |img|
         if params[:images] && params[:images]['asset'+img['assetnum']]
-          image = AttachedAsset.create(:news_id => @anecdote.id, :asset => params[:images]['asset'+img['assetnum']])
+          image = AttachedAsset.create(:anecdote_id => @anecdote.id, :asset => params[:images]['asset'+img['assetnum']])
           img.attribute('src').value = image.asset.url(:original)
           img['asset_id'] = image.id.to_s
           params[:images].delete('asset'+img['assetnum'])
@@ -29,7 +29,7 @@ class AnecdotesController < ApplicationController
 
       @anecdote.save
       flash[:success] = "Succesfully created anecdote: " + @anecdote.title
-      redirect_to 'index'
+      redirect_to anecdotes_path
     else
       flash[:success] = "Error created anecdote"
       render 'new'
@@ -37,11 +37,50 @@ class AnecdotesController < ApplicationController
   end
 
   def edit
+    @anecdote = Anecdote.find(params[:id])
+    @title = "Change anecdote"
   end
 
   def update
+    @anecdote = Anecdote.find(params[:id])
+    if @anecdote.update_attributes(params[:anecdote])
+      #save attached images
+      require 'nokogiri'
+      page =  Nokogiri::HTML(params[:anecdote][:body])
+
+      @anecdote.attached_assets.each do |image|
+        image_present = false
+        page.xpath("//img[@asset_id=#{image.id.to_s}]").each do |img|
+          image_present = true
+        end
+        unless image_present
+          image.destroy
+        end
+      end
+
+      page.xpath("//img[@asset]").each do |img|
+        if params[:images] && params[:images]['asset'+img['assetnum']]
+          image = AttachedAsset.create(:anecdote_id => @anecdote.id, :asset => params[:images]['asset'+img['assetnum']])
+          img.attribute('src').value = image.asset.url(:original)
+          img['asset_id'] = image.id.to_s
+          params[:images].delete('asset'+img['assetnum'])
+          img.remove_attribute 'assetnum'
+          img.remove_attribute 'asset'
+        end
+      end
+      @anecdote.body = page.css("body:first").inner_html
+
+      @anecdote.save
+      flash[:success] = "Succesfully changed anecdote: " + @anecdote.title
+      redirect_to anecdotes_path
+    else
+      flash[:success] = "Error changed anecdote"
+      render 'edit'
+    end
   end
 
   def destroy
+    Anecdote.find(params[:id]).destroy
+    redirect_to anecdotes_path
   end
 end
